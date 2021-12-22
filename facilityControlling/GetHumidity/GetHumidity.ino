e9de01b2b2a334458da2eee53f0f29704eeb5720
vulfjob <vulfjob@gmail.com>
2021-12-22 13:14:39 +0300

    Add humidity sensor

---
+++ facilityControlling/GetHumidity/GetHumidity.ino
@@ -0,0 +1,24 @@
+#include "DHT.h"
+#define DHTPIN 2 // Тот самый номер пина, о котором упоминалось выше
+// Одна из следующих строк закоментирована. Снимите комментарий, если подключаете датчик DHT11 к arduino
+DHT dht(DHTPIN, DHT22); //Инициация датчика
+//DHT dht(DHTPIN, DHT11);
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+void loop() {
+  delay(2000); // 2 секунды задержки
+  float h = dht.readHumidity(); //Измеряем влажность
+  float t = dht.readTemperature(); //Измеряем температуру
+  if (isnan(h) || isnan(t)) {  // Проверка. Если не удается считать показания, выводится «Ошибка считывания», и программа завершает работу
+    Serial.println("Ошибка считывания");
+    return;
+  }
+  Serial.print("Влажность: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Температура: ");
+  Serial.print(t);
+  Serial.println(" *C "); //Вывод показателей на экран
+}
